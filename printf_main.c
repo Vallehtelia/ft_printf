660ea25c39b481d2feefd70a6765ff67b4e3e4d5
Vallehtelia <141771396+Vallehtelia@users.noreply.github.com>
2023-11-22 16:38:26 +0200

    Update printf_main.c

--- printf_main.c
+++ printf_main.c
@@ -1,5 +1,5 @@
-#include "include/ft_printf.h"
 #include <stdio.h>
+#include "include/ft_printf.h"
 
 int main() {
     char c = 'A';
@@ -11,34 +11,34 @@ int main() {
     int X = 0x2A;
 
     // Testing %c
-    printf("Original printf: %c\n", c);
-    ft_printf("Custom ft_printf: %c\n", c);
+    printf("Original printf with c: %c\n", c);
+    ft_printf("Custom ft_printf with c: %c\n\n", c);
 
     // Testing %s
-    printf("Original printf: %s\n", str);
-    ft_printf("Custom ft_printf: %s\n", str);
+    printf("Original printf with s: %s\n", str);
+    ft_printf("Custom ft_printf with s: %s\n\n", str);
 
     // Testing %p
-    printf("Original printf: %p\n", ptr);
-    ft_printf("Custom ft_printf: %p\n", ptr);
+    printf("Original printf with p: %p\n", ptr);
+    ft_printf("Custom ft_printf with p: %p\n\n", ptr);
 
     // Testing %d and %i
-    printf("Original printf: %d\n", d);
-    ft_printf("Custom ft_printf: %d\n", d);
-    printf("Original printf: %i\n", d);
-    ft_printf("Custom ft_printf: %i\n", d);
+    printf("Original printf with d: %d\n", d);
+    ft_printf("Custom ft_printf with d: %d\n", d);
+    printf("Original printf with i: %i\n", d);
+    ft_printf("Custom ft_printf with i: %i\n\n", d);
 
     // Testing %u
-    printf("Original printf: %u\n", u);
-    ft_printf("Custom ft_printf: %u\n", u);
+    printf("Original printf with u: %u\n", u);
+    ft_printf("Custom ft_printf with u: %u\n\n", u);
 
     // Testing %x
-    printf("Original printf: %x\n", x);
-    ft_printf("Custom ft_printf: %x\n", x);
+    printf("Original printf with x: %x\n", x);
+    ft_printf("Custom ft_printf with x: %x\n\n", x);
 
     // Testing %X
-    printf("Original printf: %X\n", X);
-    ft_printf("Custom ft_printf: %X\n", X);
+    printf("Original printf with X: %X\n", X);
+    ft_printf("Custom ft_printf with X: %X\n\n", X);
 
     // Testing %%
     printf("Original printf: %%\n");
