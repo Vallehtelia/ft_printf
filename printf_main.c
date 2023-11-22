c34e37f2dd27f960d59d362e82a1c2aa863c239a
Vallehtelia <141771396+Vallehtelia@users.noreply.github.com>
2023-11-22 16:04:09 +0200

    Add files via upload
    printf main.c

---
+++ printf_main.c
@@ -0,0 +1,19 @@
+#include "include/ft_printf.h"
+#include "stdio.h"
+
+int main(void)
+{
+	//int a = 0;
+	//int b = 0;
+	//int real = printf("asdasd%kkckkkk");
+	int fake = ft_printf("%");
+	printf("%s", asd);
+	printf("fake %d", fake);
+
+	//a = printf("asdasd%s %p %X %10s %+d %.2f\n", "asda\0sd", NULL, 255, "test", 42, 3.14159);
+	//b = ft_printf("asdasd%s %p %X %10s %+d %.2f\n", "asda\0sd", NULL, 255, "test", 42, 3.14159);
+
+	//a = printf("asdasd%s %p %X\n", "asda\0sd", NULL, 255);
+	//b = ft_printf("asdasd%s %p %X\n", "asda\0sd", NULL, 255);
+	//printf("printf: %d ft_printf: %d\n", a, b);
+}
